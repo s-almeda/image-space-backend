2674dd59f2f1a1fd990124fc1e1dcab6f5afbbdc
shm <shm.almeda@berkeley.edu>
2025-08-25 16:15:35 -0700

    send file

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ import cors from "cors";
 import dbPromise from "./database.js";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(cors());
